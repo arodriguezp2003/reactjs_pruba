af25df686a0961ae1251733e68eae4f7db36eb2f
Alejandro Rodriguez P <alejandrorodriguezpena@gmail.com>
2017-06-06 00:53:50 -0400

    datos

--- src/clients/client.jsx
+++ src/clients/client.jsx
@@ -1,12 +1,69 @@
 import React, {Component} from 'react';
 
 export default class Client extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      client: {}
+    }
+
+
+
+    this.handleForm = this.handleForm.bind(this);
+    this.handleRut =   this.handleRut.bind(this);
+    this.handleName=   this.handleName.bind(this);
+    this.handleLastName =   this.handleLastName.bind(this);
+  }
+
+
+
+  handleForm(event) {
+     event.preventDefault();
+     console.log("Se envio el formulario", this.state.client);
+     this.props.handleClient(this.state.client);
+  }
+
+  handleRut(event) {
+    let rut = event.target.value;
+    var client = this.state.client;
+    client.rut = rut;
+    this.setState({client: client});
+
+  }
+  handleName(event) {
+    let name = event.target.value;
+    var client = this.state.client;
+    client.name = name;
+    this.setState({client: client});
+  }
+  handleLastName(event) {
+    let lt = event.target.value;
+    var client = this.state.client;
+    client.lastname = lt;
+    this.setState({client: client});
+  }
   render(){
     return(
       <div className="client">
         <h1>Nuevo Clientes</h1>
         <hr/>
-        
+        <form onSubmit={this.handleForm}>
+          <input
+            onChange={this.handleRut}
+            value={this.state.client.rut}
+            type="text"/>
+          <input
+            onChange={this.handleName}
+            value={this.state.client.name}
+            type="text"/>
+          <input
+            onChange={this.handleLastName}
+            value={this.state.client.lastname}
+            type="text"/>
+          <input type="submit"/>
+
+        </form>
       </div>
     )
   }
--- src/clients/clients.jsx
+++ src/clients/clients.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import Client from './client';
 class Clients extends React.Component {
 
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
+
     this.state = {
+      selectedClient: {},
       clients: [
         {
           rut: 16327,
@@ -17,19 +19,35 @@ class Clients extends React.Component {
           lastname: "Azocar"
         },
         {
-        rut: 16327,
+        rut: 163327,
         name: "Esteban",
         lastname: "Fajardo"
         }
       ]
     }
+    this.handleClient = this.handleClient.bind(this);
   }
 
+ handleClient(client) {
+   var clients = this.state.clients;
+   var arr = []
+
+   for(let p in clients) {
+     if(clients[p].rut == client.rut) {
+       arr.push(client)
+     } else {
+       arr.push(clients[p])
+     }
+   }
+
+   this.setState({clients: arr});
+ }
   render() {
     return(
       <div className="clients">
         <h1 className="textomasgrande">Clientes</h1>
         <hr/>
+        <Client handleClient={this.handleClient} client={this.state.selectedClient} />
         <table>
           <thead>
             <tr>
@@ -49,11 +67,16 @@ class Clients extends React.Component {
   renderTable() {
     return this.state.clients.map(client => {
       return(
-        <tr>
+        <tr key={client.rut}>
           <td>{client.rut}</td>
           <td>{client.name}</td>
           <td>{client.lastname}</td>
-          <td></td>
+          <td>
+            <button onClick={() =>{
+              console.log(client)
+              this.setState({selectedClient: client})
+            }}>Editar</button>
+          </td>
         </tr>
       )
     })
