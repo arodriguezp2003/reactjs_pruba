1d429f46a850f3cca3599de78ba1c6bce4a99c29
Alejandro Rodriguez P <alejandrorodriguezpena@gmail.com>
2017-06-06 00:12:47 -0400

    listado de clientes

--- src/clients/clients.jsx
+++ src/clients/clients.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import Client from './client';
 class Clients extends React.Component {
+
+  constructor(){
+    super();
+    this.state = {
+      clients: [
+        {
+          rut: 16327,
+          name: "Alejandro",
+          lastname: "Rodriguez"
+        },
+        {
+          rut: 16322,
+          name: "Gonzalo",
+          lastname: "Azocar"
+        },
+        {
+        rut: 16327,
+        name: "Esteban",
+        lastname: "Fajardo"
+        }
+      ]
+    }
+  }
+
   render() {
     return(
       <div className="clients">
         <h1 className="textomasgrande">Clientes</h1>
         <hr/>
-        <Client/>
         <table>
           <thead>
             <tr>
@@ -17,16 +40,23 @@ class Clients extends React.Component {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
+            {this.renderTable()}
           </tbody>
         </table>
       </div>
     )
+  }
+  renderTable() {
+    return this.state.clients.map(client => {
+      return(
+        <tr>
+          <td>{client.rut}</td>
+          <td>{client.name}</td>
+          <td>{client.lastname}</td>
+          <td></td>
+        </tr>
+      )
+    })
   }
 }
 export default Clients;
